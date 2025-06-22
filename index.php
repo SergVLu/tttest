3f5aedf3991824b3ee6c83b761b65fcceef79899
SergVLu <rumasyba@gmail.com>
2025-06-22 17:06:51 +0300

    1 manson

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ and open the template in the editor.
 <html>
   <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>MANSON</title>
   </head>
   <body>
     <?php
       echo 'gitTutorial';
-      echo 'try netbeans 8.2 git';
+      echo 'Manson git';
     ?>
   </body>
 </html>
